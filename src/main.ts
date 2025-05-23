b4bbeaf061b4379121ef9a3c2fcbc8a0f4f8fd86
Isola Pelumi <pelumiisola87@gmail.com>
2025-05-23 01:43:41 +0100

    feat: basic application set also added swagger docs

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('API for weather information and AI chat')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+bootstrap();
